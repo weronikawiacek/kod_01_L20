224c28de61b1eadc89118c43cf73d38812bcc2f6
Darek Strojecki <dstroj@wp.pl>
2020-10-29 20:04:27 +0100

    Add test constructor witch z.

--- test_point.py
+++ test_point.py
@@ -12,3 +12,15 @@ def test_should_check_constructor_without_z():
     assert point_a.x == 1
     assert point_a.y == 1
     assert point_a.z == 0
+
+
+def test_should_check_constructor_with_z():
+    # given
+    point_a = Point('A', 1, 1, 1)
+
+    # when
+    # then
+    assert point_a.name == 'A'
+    assert point_a.x == 1
+    assert point_a.y == 1
+    assert point_a.z == 1
